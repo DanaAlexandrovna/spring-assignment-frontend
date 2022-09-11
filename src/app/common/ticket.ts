6ff8ac44fda294f4c33a3aebb5ede3bdd8321cb7
Stefan-Eduard Deaconu <stefan@stefandeaconu.com>
2022-09-11 19:13:14 +0300

    Added dummy styles for the features

--- src/app/common/ticket.ts
+++ src/app/common/ticket.ts
@@ -2,4 +2,20 @@ export class Ticket {
   id!: number;
 
   // TODO we probably also need the data from the relationships with other tables
+  // DateTime
+  // user_id,
+  departure: string;
+  from: string;
+  to: string;
+  address: string
+  // user_id: number;
+  // flight_id: number;
+  // aircraft_id: number;
+}
+
+export class Address {
+  id!: number;
+  country: string;
+  address: string;
+  airport: string; // TODO SELECT
 }
--- src/app/tickets-list/tickets-list.component.ts
+++ src/app/tickets-list/tickets-list.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Ticket} from "../common/ticket";
+import {Address, Ticket} from "../common/ticket";
 import {TicketService} from "../services/ticket.service";
 import {Router} from '@angular/router';
 import {HttpErrorResponse} from "@angular/common/http";
@@ -15,8 +15,10 @@ import {Review} from "../common/review";
 export class TicketsListComponent implements OnInit {
 
   allTickets!: Ticket[];
-  destination: string = 'Honolulu';
-  arrivals: string[] = ['Tibet', 'Constanta']
+  // destination: string;
+
+  // TODO for admin/addresses
+  allAddresses!: Address[];
 
   // book!: Ticket; // delete it
   // PAGE_SIZE: number = 4;
@@ -29,28 +31,100 @@ export class TicketsListComponent implements OnInit {
   // searchType: string = "";
 
 
-  constructor(private ticketService: TicketService,
+  constructor(private bookService: TicketService,
               private router: Router,
               public commonUtil: CommonUtil) {
   }
 
   ngOnInit(): void {
     this.loadTickets();
+
+    // TODO tmp:
+    // this.allTickets = [
+    //   {
+    //     id: 1,
+    //     // user_id: 1,
+    //     departure: 'Oct 23rd 12:30 AM',
+    //     from: 'Bergamo',
+    //     to: 'Warsaw',
+    //     address: 'BGY International Airport'
+    //   },
+    //   {
+    //     id: 2,
+    //     // user_id: 1,
+    //     departure: 'Oct 27rd 8:31 AM',
+    //     from: 'Warsaw',
+    //     to: 'Bergamo',
+    //     address: ''
+    //   },
+    //   {
+    //     id: 3,
+    //     // user_id: 2,
+    //     departure: 'Nov 1st 12:30 AM',
+    //     from: 'Vienna',
+    //     to: 'Warsaw',
+    //     address: ''
+    //   },
+    //   {
+    //     id: 4,
+    //     // user_id: 2,
+    //     departure: 'Nov 2nd 2:30 AM',
+    //     from: 'Warsaw',
+    //     to: 'Helsinki',
+    //     address: ''
+    //   },
+    //   {
+    //     id: 5,
+    //     // user_id: 3,
+    //     departure: 'Nov 3rd 12:00 PM',
+    //     from: 'Milano',
+    //     to: 'Vienna',
+    //     address: ''
+    //   },
+    // ]// TODO tmp:
+
+    this.allAddresses = [
+      {
+        id: 1,
+        // user_id: 1,
+        country: 'Poland',
+        address: 'Warsaw',
+        airport: 'WAW'
+      },
+      {
+        id: 2,
+        // user_id: 2,
+        country: 'Italy',
+        address: 'Milano',
+        airport: 'MIL'
+      },
+      {
+        id: 3,
+        // user_id: 3,
+        country: 'Italy',
+        address: 'Bergamo',
+        airport: 'BGY'
+      }
+    ]
   }
 
   loadTickets() {
 
-    this.ticketService.getTickets().subscribe(
+    this.bookService.getTickets().subscribe(
       (data: any) => {
-        console.log(data)
-        this.allTickets = data.content;
+        console.log(data.content) // TODO no data found?
+        if (data.content) {
+          this.allTickets = data.content;
+        }
       }
     )
+
+
   }
 
 
   // deleteBook(id: number) {
-  //   this.ticketService.deleteBook(id).subscribe(data => {
+  //   this.bookService.deleteBook(id).subscribe(data => {
   //     console.log(data);
   //     this.getBooks();
   //   })
